e3b241c8ad7ddef69903d928df0319208bd57023
Jason Antman <jason@jasonantman.com>
2014-07-03 13:36:03 -0400

    fix specs

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,11 @@
 require 'spec_helper'
 
 describe 'reviewboard' do
+
+  let :pre_condition do
+    "package {'python-pip': ensure => present, }"
+  end
+
   context 'supported operating systems' do
     ['RedHat'].each do |osfamily|
       describe "reviewboard class without any parameters on #{osfamily}" do
@@ -11,7 +16,7 @@ describe 'reviewboard' do
 
         it { should compile.with_all_deps }
 
-        it { should contain_ckass('reviewboard::package').with_version('1.7.24') }
+        it { should contain_class('reviewboard::package').with_version('1.7.24') }
       end
     end
   end
