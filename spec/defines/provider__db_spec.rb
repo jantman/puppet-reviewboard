0ee299446630ba5a8ae8bb6c6d223bf95ce5f333
Jason Antman <Jason.Antman@coxinc.com>
2014-10-15 08:46:06 -0400

    provider::db spec

--- spec/defines/provider__db_spec.rb
+++ spec/defines/provider__db_spec.rb
@@ -3,17 +3,9 @@ require 'spec_helper'
 describe 'reviewboard::provider::db', :type => :define do
   let(:title) { '/' }
 
-  let :pre_condition do
-    <<-eos
-    package {'python-pip': ensure => present, }
-    class {'reviewboard': }
-    class {'postgresql::server': version => '9.3' }
-    eos
-  end
-
   context 'supported operating systems' do
     ['RedHat'].each do |osfamily|
-      describe "reviewboard::provider::db class without any parameters on #{osfamily}" do
+      describe "without any parameters on #{osfamily}" do
         let(:params) {{
             :dbname => 'reviewboard',
             :dbhost => 'localhost',
@@ -21,8 +13,17 @@ describe 'reviewboard::provider::db', :type => :define do
             :dbpass => 'foo'
         }}
         let(:facts) { SpecHelperFacts.new({:osfamily => osfamily}).facts }
+        let :pre_condition do
+          <<-eos
+          package {'python-pip': ensure => present, }
+          class {'reviewboard': }
+          class {'postgresql::server': version => '9.3' }
+          eos
+        end
 
         it { should compile.with_all_deps }
+
+        it { should_not contain_reviewboard__provider__db__puppetlabsmysql('/') }
 
         it { should contain_reviewboard__provider__db__puppetlabspostgresql('/').with({
                                                                                         :dbname => 'reviewboard',
@@ -31,6 +32,80 @@ describe 'reviewboard::provider::db', :type => :define do
                                                                                         :dbpass => 'foo'
                                                                                       })
         }
+      end
+
+      describe "with mysql dbprovider on #{osfamily}" do
+        let(:params) {{
+            :dbname => 'reviewboard',
+            :dbhost => 'localhost',
+            :dbuser => 'reviewboard',
+            :dbpass => 'foo'
+        }}
+        let(:facts) { SpecHelperFacts.new({:osfamily => osfamily}).facts }
+        let :pre_condition do
+          <<-eos
+          package {'python-pip': ensure => present, }
+          class {'reviewboard': dbprovider => 'puppetlabs/mysql',}
+          class {'postgresql::server': version => '9.3' }
+          eos
+        end
+
+        it { should compile.with_all_deps }
+
+        it { should_not contain_reviewboard__provider__db__puppetlabspostgresql('/') }
+
+        it { should contain_reviewboard__provider__db__puppetlabsmysql('/').with({
+                                                                                   :dbname => 'reviewboard',
+                                                                                   :dbhost => 'localhost',
+                                                                                   :dbuser => 'reviewboard',
+                                                                                   :dbpass => 'foo'
+                                                                                 })
+        }
+      end
+
+      describe "with none dbprovider on #{osfamily}" do
+        let(:params) {{
+            :dbname => 'reviewboard',
+            :dbhost => 'localhost',
+            :dbuser => 'reviewboard',
+            :dbpass => 'foo'
+        }}
+        let(:facts) { SpecHelperFacts.new({:osfamily => osfamily}).facts }
+        let :pre_condition do
+          <<-eos
+          package {'python-pip': ensure => present, }
+          class {'reviewboard': dbprovider => 'none',}
+          class {'postgresql::server': version => '9.3' }
+          eos
+        end
+
+        it { should compile.with_all_deps }
+
+        it { should_not contain_reviewboard__provider__db__puppetlabsmysql('/') }
+        it { should_not contain_reviewboard__provider__db__puppetlabspostgresql('/') }
+      end
+
+      describe "with other/invalid dbprovider on #{osfamily}" do
+        let(:params) {{
+            :dbname => 'reviewboard',
+            :dbhost => 'localhost',
+            :dbuser => 'reviewboard',
+            :dbpass => 'foo'
+        }}
+        let(:facts) { SpecHelperFacts.new({:osfamily => osfamily}).facts }
+        let :pre_condition do
+          <<-eos
+          package {'python-pip': ensure => present, }
+          class {'reviewboard': dbprovider => 'invaild',}
+          class {'postgresql::server': version => '9.3' }
+          eos
+        end
+
+        it do
+          expect {
+            should contain_reviewboard__provider__web('sitename')
+          }.to raise_error(Puppet::Error, /DB provider 'invaild' not defined/)
+        end
       end
     end
   end
