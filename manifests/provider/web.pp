df74f9a88bae1c218cfaa714e723cb25e9848bee
Jason Antman <jason@jasonantman.com>
2014-10-30 14:32:39 -0400

    forgot to set perms on log directory

--- manifests/provider/web.pp
+++ manifests/provider/web.pp
@@ -79,7 +79,7 @@ define reviewboard::provider::web (
   }
 
   # Set web folder ownership
-  file {["${site}/data", "${site}/htdocs/media", "${site}/htdocs/media/ext"]:
+  file {["${site}/data", "${site}/htdocs/media", "${site}/htdocs/media/ext", "${site}/logs"]:
     ensure  => directory,
     owner   => $realwebuser,
     notify  => $webservice,
